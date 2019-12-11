b77a8972d3af2cd1842b9113e448be33d9ca0462
Yasin Shamrat <yshamrat@gmail.com>
2019-12-11 09:36:10 +0600

    Initial Commit

---
+++ Table View Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View Practice
+//
+//  Created by Leads Computer on 12/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
